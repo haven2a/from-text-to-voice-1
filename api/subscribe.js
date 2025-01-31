a15aea75d6202143ee4d1b26e13104c5bccee689
abouhoussem <abohousseme@gmail.com>
2025-01-31 17:00:14 +0100

    Add files via upload

---
+++ api/subscribe.js
@@ -0,0 +1,8 @@
+module.exports = async (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: '⚠️ الميثود غير مدعومة' });
+    }
+
+    console.log('📩 بيانات التسجيل:', req.body);
+    res.status(201).json({ message: '✅ تم التسجيل بنجاح!' });
+};
