1bd548a752dc8739ca18419caa28317998f7c4bf
abouhoussem <abohousseme@gmail.com>
2025-02-01 17:31:25 +0100

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,8 +1,8 @@
+// pages/api/subscribe.js
 module.exports = async (req, res) => {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: '⚠️ الميثود غير مدعومة' });
     }
-
     console.log('📩 بيانات التسجيل:', req.body);
     res.status(201).json({ message: '✅ تم التسجيل بنجاح!' });
 };
